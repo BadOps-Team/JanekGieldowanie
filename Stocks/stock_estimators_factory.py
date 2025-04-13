92790a630ac81bfa3ed8669db4cf0b0aa96a124e
kawixKrul <macioukos@gmail.com>
2025-04-13 14:30:03 +0200

    added estimators

---
+++ Stocks/stock_estimators_factory.py
@@ -0,0 +1,37 @@
+from Stocks.stock_price_estimator import *
+from Stocks.stock_utility import StockUtility
+from enum import Enum
+from typing import Self
+
+class EstimatorStrategy(Enum):
+    METHOD_OF_MOMENTS = 1
+    MAXIMUM_LIKELIHOOD = 2
+    LEAST_SQUARE_METHOD = 3
+
+class EstimatorBuilder:
+    def __init__(self) -> None:
+        self.strategy = None
+        self.stock_util = None
+    
+    @staticmethod
+    def create_builder() -> Self:
+        return EstimatorBuilder()
+
+    def with_startegy(self, strategy: EstimatorStrategy) -> Self:
+        self.strategy = strategy
+        return self
+    
+    def with_stock_util(self, stock_util: StockUtility) -> Self:
+        self.stock_util = stock_util
+        return self
+    
+    def build(self) -> StockPriceEstimator:
+        match self.strategy:
+            case EstimatorStrategy.METHOD_OF_MOMENTS:
+                return MethodOfMomentsEstimator(self.stock_util)
+            case EstimatorStrategy.MAXIMUM_LIKELIHOOD:
+                return MaximumLikelihoodEstimator(self.stock_util)
+            case EstimatorStrategy.LEAST_SQUARE_METHOD:
+                return LeastSqaureMethodEstimator(self.stock_util)
+            case _:
+                raise NotImplementedError()
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from Stocks.period import Period
 from datetime import date
 from Stocks.stock_utility import StockUtility
+from Stocks.stock_estimators_factory import *
 
 def main():
-    period = Period(date(2010, 1, 1), date(2020, 1, 1)) 
+    period = Period(date(2019, 1, 1), date(2020, 1, 1)) 
     print(period)
     apple_stock =  StockUtility('AAPL')
     apple_data = apple_stock.get_historical_data(period=period)
-    print(apple_data)
-    apple_stock.save_data()
+    print(apple_data.Close)
+    estimator = (EstimatorBuilder.create_builder()
+                 .with_startegy(EstimatorStrategy.LEAST_SQUARE_METHOD)
+                 .with_stock_util(apple_stock)
+                 .build())
+    results = estimator.estimate_from_historic(period, 10)
+    print(results)
+    
 
 if __name__ == "__main__":
     main()
