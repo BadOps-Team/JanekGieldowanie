89f3ccf9facdc01adfeaaa884e512fa18b3e0773
kawixKrul <macioukos@gmail.com>
2025-04-21 14:32:41 +0200

    better stock util

---
+++ Stocks/__init__.py
@@ -0,0 +1,5 @@
+from .stock_utility import StockUtility
+from .period import Period
+from .stock_utility_factory import StockUtilityFactory
+
+__all__ = ['StockUtility', 'Period', 'StockUtilityFactory']
