f8b95aab30ce7e1e2c6d9effa52fd3f0fb49c1c5
mbartnicki80 <mateuszbartnickiq@gmail.com>
2025-04-21 17:00:59 +0200

    add profit matrix

--- Agent/agent.py
+++ Agent/agent.py
@@ -1,10 +1,10 @@
 import random
 from collections import defaultdict
-
+import numpy as np
 
 class Agent:
     def __init__(self, asset, minimum_holding_period, probability_distribution=lambda: random.uniform(0, 1), max_loss=0.2, strategy='basic',
-                 minimum_bought=5, stop_loss=0.9, take_profit=1.2):
+                 minimum_bought=5, stop_loss=0.9, take_profit=1.2, history_length = 10):
         self.START_ASSET = asset  # const
         self.curr_asset = asset
         self.probability_distribution = probability_distribution
@@ -16,10 +16,40 @@ class Agent:
         self.minimum_bought = minimum_bought
         self.stop_loss = stop_loss
         self.take_profit = take_profit
+        self.history_length = history_length
+        self.history_prices = defaultdict(list)
+
+    def update_history(self, current_prices: dict):
+        for ticker, price in current_prices.items():
+            self.history_prices[ticker].append(price)
+            if len(self.history_prices[ticker]) > self.history_length:
+                self.history_prices[ticker].pop(0)
+
+    @staticmethod
+    def calculate_eigenvalues(sigma): # musi być macierz kwadratowa!!!
+        return np.linalg.eigvalsh(sigma)
+
+    def create_sigma_matrix(self):
+        sigma = []
+        for i, (ticker, prices) in enumerate(self.history_prices):
+            sigma.append(prices)
+            if len(prices) < self.history_length:
+                for _ in range(self.history_length - len(prices)):
+                    sigma[i].append(0)
+
+        for _ in range(self.history_length - len(sigma)):
+            sigma.append(np.zeros(self.history_length))
+
+        return sigma
+
+    def calculate_F(self, X_prime: list):
+        t = len(X_prime)
+        if t == 0:
+            return 0
+        return np.sum(X_prime) / np.sqrt(t)
 
     def make_decision(self, current_prices: dict, day: int) -> dict:
         decisions = {}
-
 
         while sum(len(v) for v in self.bought.values()) < self.minimum_bought:
             ticker = random.choice(list(current_prices.keys())) # gamblujemy decyzje es
