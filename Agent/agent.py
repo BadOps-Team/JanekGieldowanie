816188037b6cb2e50ba57f99da702771e7f6893e
nr6000000 <kgrybek1@gmail.com>
2025-05-01 08:57:47 +0200

    small improvement

--- Agent/agent.py
+++ Agent/agent.py
@@ -1,6 +1,12 @@
 class Agent:
     def __init__(self, sale_history):
+        # Taki słownik: {
+        #   'ticker1': [1, 2, -1, -2, 3] <- Tyle elementów listy ile dni, dodatni to kupno, ujemny to sprzedaż
+        #   'ticker2': [9, 2, -2, 3, 0] 
+        # }
         self.sale_history = sale_history
+        
+        
         self.profit = 0
 
     def execute(self):
