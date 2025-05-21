f55e366b9babd46984919e2dc015883c494ae366
Hubert Tułacz <htulacz@student.agh.edu.pl>
2025-05-22 01:19:25 +0200

    commit{0}

--- util.sh
+++ util.sh
@@ -7,4 +7,18 @@ elif [ "$1" == "generate" ]; then
 	for ((i=1; i <= $2; i++)); do
 		cp configs/config${i}_1.json configs/config${i}_4.json
 	done
+elif [ "$1" == "commit_machine" ]; then
+	for ((i=0; i < $2; i++)); do
+		if ((i%2 == 0 )); then
+			echo "pzdr" > temp
+			git add .
+			git commit -m "commit{$i}"
+			git push
+		else 
+			rm -f temp
+			git add .
+			git commit -m "commit{$i}"
+			git push
+		fi
+	done
 fi
